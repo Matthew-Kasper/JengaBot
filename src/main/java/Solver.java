9f45dbaabbbb27f263f3e7ddf81c7c0f243001be
CoolPolyester <Mkasper045@gmail.com>
2023-07-04 15:30:22 -0700

    Create initial constructor for solver

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -1,4 +1,14 @@
 public class Solver {
 
-    public Solver()
+    // Tower that represents playing field
+    private Tower tower;
+
+    // If computer moves first
+    private boolean isFirstMove;
+
+    // Input the play tower to initialize solver
+    public Solver(Tower tower, boolean isFirstMove) {
+        this.tower = tower;
+        this.isFirstMove = isFirstMove;
+    }
 }
