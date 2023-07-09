7a1862c119429d5417758c067ec1c7484320e8c4
CoolPolyester <Mkasper045@gmail.com>
2023-07-09 16:26:47 -0700

    Create code to generate a tower automatically based on a tower object

--- src/main/java/Physics.java
+++ src/main/java/Physics.java
@@ -28,7 +28,7 @@ public class Physics {
         data.getSpace().collide(data, new CollisionsCallback());
 
         // Run simulation
-        data.getWorld().quickStep(Constants.SIM_STEP_SIZE);
+        data.getWorld().step(Constants.SIM_STEP_SIZE);
 
         // Remove contact joints
         data.getContactGroup().empty();
@@ -37,7 +37,7 @@ public class Physics {
     /**
      * Initializes a simulation and returns references to key data
      */
-    public static SimulationData createSimulation() {
+    public static SimulationData createSimulation(Tower tower) {
         // Create a simulation space and add boxes to create a Jenga tower
         OdeHelper.initODE2(0);
 
@@ -56,17 +56,66 @@ public class Physics {
         // Set the world gravity
         world.setGravity(0, Constants.GRAVITY_Y, 0);
 
-        // Create a piece
-        PhysicsPiece piece1 = new PhysicsPiece(world, space);
-
-        // Put piece in the air
-        piece1.setPosition(0, 1, 0);
+        ArrayList<Boolean[]> pieces = tower.getPieces();
 
         // Create a structure to store the pieces
-        ArrayList<PhysicsPiece[]> physicsPieces = new ArrayList<PhysicsPiece[]>();
+        ArrayList<PhysicsPiece[]> physicsPieces = new ArrayList<PhysicsPiece[]>(pieces.size());
 
-        // Add the piece to the structure
-        physicsPieces.add(new PhysicsPiece[]{piece1, null, null});
+        // Adds a piece in the position corresponding to the piece of the tower
+        for (int i = 0; i < pieces.size(); i++) {
+            // Create a layer for holding physics pieces
+            PhysicsPiece[] physicsLayer = new PhysicsPiece[3];
+            physicsPieces.add(i, physicsLayer);
+
+            // Get the height to place a piece on a given layer at
+            double pieceHeight = (Constants.PIECE_HEIGHT / 2.0) + (Constants.PIECE_HEIGHT * i) + Constants.BLOCK_SEPARATION_VERT;
+
+            // Iterate through each of the pieces in the layer
+            for (int j = 0; j < physicsLayer.length; j++) {
+
+                // If the piece exists, create it
+                if (pieces.get(i)[j]) {
+                    // Create a new physics piece
+                    PhysicsPiece piece = new PhysicsPiece(world, space);
+                    // Add piece to physics layer
+                    physicsLayer[j] = piece;
+
+                    // If the layer is odd, turn the piece 90 degrees
+                    if (i % 2 == 0) {
+                        piece.turn90();
+                        switch (j) {
+                            case 0:
+                                // If on left side
+                                piece.setPosition(0, pieceHeight, Constants.PIECE_WIDTH + Constants.BLOCK_SEPARATION_HORIZ);
+                                break;
+                            case 1:
+                                // If in middle
+                                piece.setPosition(0, pieceHeight, 0);
+                                break;
+                            case 2:
+                                // If on right side
+                                piece.setPosition(0, pieceHeight, -Constants.PIECE_WIDTH - Constants.BLOCK_SEPARATION_HORIZ);
+                                break;
+                        }
+                    } else {
+                        switch (j) {
+                            case 0:
+                                // If on left side
+                                piece.setPosition(-Constants.PIECE_WIDTH - Constants.BLOCK_SEPARATION_HORIZ, pieceHeight, 0);
+                                break;
+                            case 1:
+                                // If in middle
+                                piece.setPosition(0, pieceHeight, 0);
+                                break;
+                            case 2:
+                                // If on right side
+                                piece.setPosition(Constants.PIECE_WIDTH + Constants.BLOCK_SEPARATION_HORIZ, pieceHeight, 0);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
 
         return new SimulationData(space, world, contactGroup, physicsPieces);
     }
