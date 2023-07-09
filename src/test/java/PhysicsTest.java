3672ec352bfdbc78c2c8a41329df3bee9b80419b
CoolPolyester <Mkasper045@gmail.com>
2023-07-09 16:26:59 -0700

    Modify physics tests

--- src/test/java/PhysicsTest.java
+++ src/test/java/PhysicsTest.java
@@ -4,22 +4,38 @@ public class PhysicsTest {
 
     @Test
     public void TowerSimulationTest() {
-        SimulationData simData = Physics.createSimulation();
+        Tower tower = new Tower(6);
+
+        SimulationData simData = Physics.createSimulation(tower);
 
         // Take a piece to track
-        PhysicsPiece piece = simData.getPhysicsPieces().get(0)[0];
+        PhysicsPiece piece = simData.getPhysicsPieces().get(5)[1];
 
         // Print height of tracked
-        System.out.println("Initial Position: " + "Height: " + piece.getPosition()[1]);
+        System.out.println("Initial Position:" + " X: " + piece.getPosition()[0] + " Y: " + piece.getPosition()[1] + " Z: " + piece.getPosition()[2]);
 
         // Run simulation n times
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             Physics.simulate(simData);
 
             // Print height of tracked
-            System.out.println("Iteration: " + i + " Height: " + piece.getPosition()[1]);
+            System.out.println("Iteration: " + i + " X: " + piece.getPosition()[0] + " Y: " + piece.getPosition()[1] + " Z: " + piece.getPosition()[2]);
         }
 
         Physics.terminateSimulation(simData);
+    }
+
+    @Test
+    public void TowerBuildTest() {
+        Tower tower = new Tower(18);
+
+        SimulationData simulationData = Physics.createSimulation(tower);
+
+        for (int i = 0; i < tower.getPieces().size(); i++) {
+            for (int j = 0; j < tower.getPieces().get(i).length; j++) {
+                PhysicsPiece piece = simulationData.getPhysicsPieces().get(i)[j];
+                System.out.println("Piece: " + i + " " + j + " at " + " X: " + piece.getPosition()[0] + " Y: " + piece.getPosition()[1] + " Z: " + piece.getPosition()[2]);
+            }
+        }
     }
 }
