7fe12835b4e33eda6be4480704985d27d0f9dbe4
CoolPolyester <Mkasper045@gmail.com>
2023-07-09 20:41:21 -0700

    Create a solver test to test normal use.

---
+++ src/test/java/SolverTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class SolverTest {
+    @Test
+    public void TestInit() {
+        Solver solver = new Solver(18);
+        solver.doHumanMove(0, 1, 18, 0);
+        System.out.println("Is lowest layer empty: " + Arrays.equals(solver.getCurrentTower().get(0), Constants.DEFAULT_EMPTY_LAYER));
+
+    }
+}
